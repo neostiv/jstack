2d54555a0b9c77f3641a7fd3706f3e92012e41ff
John Torres <neostiv@gmail.com>
2025-02-05 15:48:04 -0500

    ChatShowsProfileDetailsofChatters OK

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,7 +3,8 @@ import { MaxWidthWrapper } from "@/components/max-width-wrapper"
 import { Check } from "lucide-react"
 import { ShinyButton } from "@/components/shiny-button"
 import { MockDiscordUI } from "@/components/mock-discord-ui"
-
+import { AnimatedList, AnimatedListItem } from "@/components/ui/animated-list"
+import { DiscordMessage } from "@/components/discord-message"
 
 const Page = () => {
   return (
@@ -54,17 +55,45 @@ const Page = () => {
         </MaxWidthWrapper>
       </section>
 
-
       <section className="relative bg-brand-25 pb-4">
-              <div className="absolute inset-x-0 bottom-24 top-24 bg-brand-700" />
-              <div className="relative mx-auto">
-                <MaxWidthWrapper className="relative">
-                  <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-                      <MockDiscordUI>Jesus</MockDiscordUI>
-                  </div>
-                </MaxWidthWrapper>
-              </div>
-
+        <div className="absolute inset-x-0 bottom-24 top-24 bg-brand-700" />
+        <div className="relative mx-auto">
+          <MaxWidthWrapper className="relative">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <MockDiscordUI>
+                {" "}
+                <AnimatedList>
+                  <DiscordMessage
+                    avatarSrc="/brand-asset-profile-picture.png"
+                    avatarAlt="PingPanda Avatar"
+                    username="PeeeengPanda"
+                    timestamp="today at 12:35PM"
+                    badgeText="SignUp"
+                    badgeColor="#43b581"
+                    title="🤑 New User Sign Up & Make Money"
+                    content={{
+                      name: "Felixito Te felicito",
+                      email: "raizafelixtobias1ro2do@gmail.com",
+                    }}
+                  />
+                  <DiscordMessage
+                    avatarSrc="/brand-asset-profile-picture.png"
+                    avatarAlt="PingPanda Avatar"
+                    username="PeeeengPanda"
+                    timestamp="today at 12:35PM"
+                    badgeText="SignUp"
+                    badgeColor="#43b581"
+                    title="🤑 New User Sign Up & Make Money"
+                    content={{
+                      name: "Felixito Te felicito",
+                      email: "raizafelixtobias1ro2do@gmail.com",
+                    }}
+                  />
+                </AnimatedList>
+              </MockDiscordUI>
+            </div>
+          </MaxWidthWrapper>
+        </div>
       </section>
       <section></section>
       <section></section>
