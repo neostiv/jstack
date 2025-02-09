27c7f0fec8e293a18d9a0d727cc44f3c9b778e57
John Torres <neostiv@gmail.com>
2025-02-08 23:57:37 -0500

    Bento Grid Eventtraker OK

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -5,7 +5,7 @@ import { ShinyButton } from "@/components/shiny-button"
 import { MockDiscordUI } from "@/components/mock-discord-ui"
 import { AnimatedList, AnimatedListItem } from "@/components/ui/animated-list"
 import { DiscordMessage } from "@/components/discord-message"
-
+import Image from "next/image"
 const Page = () => {
   return (
     <>
@@ -109,7 +109,75 @@ const Page = () => {
           </MaxWidthWrapper>
         </div>
       </section>
-      <section></section>
+
+      <section className="relative py-24 sm:py-32 bg-brand-25">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-20">
+          {""}
+          <div>
+            <h2 className="text-center text-base/- font-semibold text-brand-600">
+              Intuitive Monitoring
+            </h2>
+            <Heading>Stay Way Ahead with real insights</Heading>
+          </div>
+
+          <div className="grid gap-4 lg:grid-cols-3 lg:grid-rows-2">
+            {/*first bento grid element*/}
+            <div className="relative lg:row-span-2">
+              <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]" />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc((2rem+1px))]">
+                <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                  <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
+                    Realtime NotifiCAT
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                    Get notified if you are on-duty or at your cabin, remember
+                    crew drills, pax drills and pax briefing
+                  </p>
+                </div>
+
+                <div className="relative min-h-[30rem] w-full grow [container-type:inline-size] max-lg:mx-auto max-lg:max-w-sm">
+                  <div className="absolute inset-x-10 bottom-0 top-10 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
+                    <Image
+                      className="size-full object-cover object-top"
+                      src="/phone-screen.png"
+                      alt="Phone screen displaying App interface"
+                      fill
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]" />
+            </div>
+
+            {/*Second Bento Grid Element */}
+            <div className="relative max-lg:row-start-1">
+              <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]" />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1)] max-lg:rounded-t-[calc(2rem+1px)]">
+                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                  <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
+                    Track Any Meaningful Trend
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                    From New embarking crew members to sucessfull payments
+                    peeengPanda notifies you for all critical happenings in your
+                    SaaS
+                  </p>
+                </div>
+                <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
+                  <Image
+                    className="w-full max-lg:max-w-xs"
+                    src="/bento-any-event.png"
+                    alt="Bento box illustrating event tracking"
+                    width={500}
+                    height={300}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
       <section></section>
       <section></section>
     </>
