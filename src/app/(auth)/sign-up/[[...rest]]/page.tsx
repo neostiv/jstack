346e576a5a65cf5f167cf826657a8dc59ba882d8
John Torres <neostiv@gmail.com>
2025-02-15 01:04:36 -0500

    Autenticacion Clerk loginsignout OK

---
+++ src/app/(auth)/sign-up/[[...rest]]/page.tsx
@@ -0,0 +1,14 @@
+"use-client"
+
+import { SignUp } from "@clerk/nextjs"
+
+const Page = () => {
+  return (
+    <div className="w-full flex-1 flex items-center justify-center">
+      <SignUp />
+      Lorem Ipsum
+    </div>
+  )
+}
+
+export default Page
